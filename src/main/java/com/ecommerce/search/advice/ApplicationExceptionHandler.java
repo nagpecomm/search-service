e0d695f72ea70457a0ef4fdb89eba4a90e108eb9
Mahesh Tambe <maheshtambelab@gmail.com>
2026-03-12 19:25:50 +0530

    search service

--- src/main/java/com/ecommerce/search/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/ecommerce/search/advice/ApplicationExceptionHandler.java
@@ -2,16 +2,19 @@ package com.ecommerce.search.advice;
 
 import com.ecommerce.search.exceptions.BadRequestException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
 
-    @ExceptionHandler(BadRequestException.class) // RFC9457
-    public ProblemDetail handleException(BadRequestException ex){
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleException(BadRequestException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
     }
 
 }
